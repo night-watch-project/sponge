a64c5849fc396023e3def74cbb81575473a7d301
Lương Quang Mạnh <luongquangmanh85@gmail.com>
2020-05-29 22:01:11 +0700

    Add rate limiter

--- api/scraper.js
+++ api/scraper.js
@@ -1,20 +1,25 @@
 const { HeadlessBrowser } = require("../lib/headless-browser")
-
-const browser = new HeadlessBrowser()
+const { RateLimiter } = require("../lib/rate-limiter")
 
 module.exports = async (req, res) => {
   try {
+    await RateLimiter.consume(req)
+  } catch {
+    return res.status(429).json({})
+  }
+
+  try {
     if (req.method === "GET") {
-      res.status(200).json({ iam: "/api/scraper" })
+      return res.status(200).json({ iam: "/api/scraper" })
     } else if (req.method === "POST") {
       const { url, targets } = req.body
-      const resultTargets = await browser.scrape(url, targets)
-      res.status(200).json({ targets: resultTargets })
+      const resultTargets = await HeadlessBrowser.scrape(url, targets)
+      return res.status(200).json({ targets: resultTargets })
     } else {
-      res.status(405).json({})
+      return res.status(405).json({})
     }
   } catch (err) {
     console.error(err)
-    res.status(500).json({})
+    return res.status(500).json({})
   }
 }
