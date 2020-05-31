6007db6d0c3059305a2e2d61e955677e44d9a82a
Lương Quang Mạnh <luongquangmanh85@gmail.com>
2020-05-31 19:16:34 +0700

    Setup Saasify

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { NestFactory } from "@nestjs/core"
 import { FastifyAdapter } from "@nestjs/platform-fastify"
 import type { NestFastifyApplication } from "@nestjs/platform-fastify"
 import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger"
+import { promises as fs } from "fs"
+import * as path from "path"
 import * as packagejson from "../package.json"
 import { AppModule } from "./app.module"
 
@@ -15,9 +17,13 @@ const bootstrap = async () => {
     .setTitle(packagejson.name)
     .setDescription(packagejson.description)
     .setVersion(packagejson.version)
+    .addServer("http://localhost:3000")
+    .addServer("https://github.com/night-watch-project/spider-ham")
     .build()
   const document = SwaggerModule.createDocument(app, options)
   SwaggerModule.setup("docs", app, document)
+
+  await fs.writeFile(path.join(__dirname, "../openapi.json"), JSON.stringify(document))
 
   await app.listen(3000)
 }
--- src/scraper/scraper.controller.ts
+++ src/scraper/scraper.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post, Query } from "@nestjs/common"
+import { Body, Controller, Get, Post } from "@nestjs/common"
 import { CommandScrapeDto } from "./dto/command-scrape.dto"
 import { ScrapeResultDto } from "./dto/scrape-result.dto"
 import { ScraperService } from "./scraper.service"
@@ -8,13 +8,12 @@ export class ScraperController {
   constructor(private readonly service: ScraperService) {}
 
   @Get()
-  async scrapeWithGet(@Query() query: CommandScrapeDto): Promise<ScrapeResultDto> {
-    const targets = await this.service.run(query.url, query.targets ?? [])
-    return { targets }
+  get(): { iam: "/scraper" } {
+    return { iam: "/scraper" }
   }
 
   @Post()
-  async scrapeWithPost(@Body() body: CommandScrapeDto): Promise<ScrapeResultDto> {
+  async post(@Body() body: CommandScrapeDto): Promise<ScrapeResultDto> {
     const targets = await this.service.run(body.url, body.targets ?? [])
     return { targets }
   }
