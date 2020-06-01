1d8223241e54f400fcdad347bb403bc3456b8eb1
Lương Quang Mạnh <luongquangmanh85@gmail.com>
2020-06-01 14:25:26 +0700

    Add dotenv

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,14 @@ import { NestFactory } from "@nestjs/core"
 import { FastifyAdapter } from "@nestjs/platform-fastify"
 import type { NestFastifyApplication } from "@nestjs/platform-fastify"
 import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger"
+import * as dotenv from "dotenv-flow"
 import { promises as fs } from "fs"
 import * as path from "path"
 import * as packagejson from "../package.json"
 import { AppModule } from "./app.module"
+
+dotenv.config()
+const { HTTPS = "false", HOST = "localhost", PORT = "3000" } = process.env
 
 const bootstrap = async () => {
   const app = await NestFactory.create<NestFastifyApplication>(
@@ -17,14 +21,13 @@ const bootstrap = async () => {
     .setTitle(packagejson.name)
     .setDescription(packagejson.description)
     .setVersion(packagejson.version)
-    .addServer("http://localhost:3000")
-    .addServer("https://httpbin.org")
+    .addServer(`${HTTPS === "true" ? "https" : "http"}://${HOST}:${PORT}`)
     .build()
   const document = SwaggerModule.createDocument(app, options)
   SwaggerModule.setup("docs", app, document)
   // export OpenAPI/Swagger specs to JSON file, in order to work with Saasify
   await fs.writeFile(path.join(__dirname, "../openapi.json"), JSON.stringify(document))
 
-  await app.listen(3000)
+  await app.listen(parseInt(PORT))
 }
 bootstrap()
