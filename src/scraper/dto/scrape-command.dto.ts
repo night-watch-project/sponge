429d0aeb444eb8956c832807a79687362904ca0b
Lương Quang Mạnh <luongquangmanh85@gmail.com>
2020-06-16 02:11:21 +0700

    Fix @nestjs/swagger doesn't recognize boolean

--- src/scraper/dto/scrape-command.dto.ts
+++ src/scraper/dto/scrape-command.dto.ts
@@ -1,10 +1,14 @@
+import { ApiProperty } from "@nestjs/swagger"
 import { HttpProxy } from "../../common/types/http-proxy.class"
 import { InputTarget } from "../types/input-target.class"
 
 export class ScrapeCommandDto {
   url: string
   targets?: InputTarget[] = []
+
+  @ApiProperty({ type: "boolean" }) // manually annotated as @nestjs/swagger doesn't recognize boolean
   metadata?: boolean = false
+
   headers?: Record<string, string>
   httpProxy?: HttpProxy
 }
