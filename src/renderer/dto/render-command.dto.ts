cd69eae12a509923e358380c15db47182dda5859
Lương Quang Mạnh <luongquangmanh85@gmail.com>
2020-08-03 23:54:38 +0700

    Upgrade NestJS dependencies

--- src/renderer/dto/render-command.dto.ts
+++ src/renderer/dto/render-command.dto.ts
@@ -1,12 +1,8 @@
-import { ApiProperty } from "@nestjs/swagger"
 import { HttpProxy } from "../../common/types/http-proxy.class"
 
 export class RenderCommandDto {
   url: string
-
-  @ApiProperty({ type: "boolean" }) // manually annotated as @nestjs/swagger doesn't recognize boolean
   blockAds?: boolean = true
-
   headers?: Record<string, string>
   httpProxy?: HttpProxy
 }
