822a2141598258868093779ed04eff3d48666bac
Lương Quang Mạnh <luongquangmanh85@gmail.com>
2020-08-14 01:05:08 +0700

    URL validation

--- src/scraper/dto/scrape-command.dto.ts
+++ src/scraper/dto/scrape-command.dto.ts
@@ -1,8 +1,11 @@
+import { IsUrl } from "class-validator"
 import { HttpProxy } from "../../common/types/http-proxy.class"
 import { InputTarget } from "../types/input-target.class"
 
 export class ScrapeCommandDto {
+  @IsUrl()
   url: string
+
   targets?: InputTarget[] = []
   metadata?: boolean = false
   blockAds?: boolean = true
