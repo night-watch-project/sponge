40424bda4da281b4e6dd13610d5c60b8fcc738c5
Lương Quang Mạnh <luongquangmanh85@gmail.com>
2020-08-22 21:36:23 +0700

    Complete

--- src/renderer/renderer.controller.ts
+++ src/renderer/renderer.controller.ts
@@ -1,38 +1,60 @@
-import { Body, Controller, Get, Header, Post, Query } from "@nestjs/common"
+import { Body, Controller, Get, Header, Post, Query, Res } from "@nestjs/common"
+import { ConfigService } from "@nestjs/config"
+import type { FastifyReply } from "fastify"
 import { RenderCommandDto } from "./dto/render-command.dto"
 import { RenderQueryDto } from "./dto/render-query.dto"
 import { RendererService } from "./renderer.service"
 
 @Controller("v1/renderer")
 export class RendererController {
-  constructor(private readonly renderer: RendererService) {}
+  private readonly quotaCostHeader: string
+  private readonly quotaName: string
+
+  constructor(config: ConfigService, private readonly renderer: RendererService) {
+    this.quotaCostHeader = config.get<string>("QUOTA_COST_HEADER") as string
+    this.quotaName = config.get<string>("QUOTA_NAME", "Credits")
+  }
 
   @Get("csr")
   @Header("Content-Type", "text/html; charset=UTF-8")
-  getCSR(@Query() query: RenderQueryDto): Promise<string> {
-    return this.renderer.renderCSR(query.url, query.blockAds ?? true)
+  async getCSR(@Query() query: RenderQueryDto, @Res() res: FastifyReply): Promise<void> {
+    const quotas = 5
+    res
+      .header(this.quotaCostHeader, `${this.quotaName}=${quotas}`)
+      .send(await this.renderer.renderCSR(query.url, query.blockAds ?? true))
   }
 
   @Post("csr")
   @Header("Content-Type", "text/html; charset=UTF-8")
-  postCSR(@Body() body: RenderCommandDto): Promise<string> {
-    return this.renderer.renderCSR(
-      body.url,
-      body.blockAds ?? true,
-      body.headers,
-      body.httpProxy
-    )
+  async postCSR(@Body() body: RenderCommandDto, @Res() res: FastifyReply): Promise<void> {
+    const quotas = body.httpProxy ? 6 : 5
+    res
+      .header(this.quotaCostHeader, `${this.quotaName}=${quotas}`)
+      .send(
+        await this.renderer.renderCSR(
+          body.url,
+          body.blockAds ?? true,
+          body.headers,
+          body.httpProxy
+        )
+      )
   }
 
   @Get("ssr")
   @Header("Content-Type", "text/html; charset=UTF-8")
-  getSSR(@Query() query: RenderQueryDto): Promise<string> {
-    return this.renderer.renderSSR(query.url)
+  async getSSR(@Query() query: RenderQueryDto, @Res() res: FastifyReply): Promise<void> {
+    const quotas = 1
+    res
+      .header(this.quotaCostHeader, `${this.quotaName}=${quotas}`)
+      .send(await this.renderer.renderSSR(query.url))
   }
 
   @Post("ssr")
   @Header("Content-Type", "text/html; charset=UTF-8")
-  postSSR(@Body() body: RenderCommandDto): Promise<string> {
-    return this.renderer.renderSSR(body.url, body.headers, body.httpProxy)
+  async postSSR(@Body() body: RenderCommandDto, @Res() res: FastifyReply): Promise<void> {
+    const quotas = body.httpProxy ? 2 : 1
+    res
+      .header(this.quotaCostHeader, `${this.quotaName}=${quotas}`)
+      .send(await this.renderer.renderSSR(body.url, body.headers, body.httpProxy))
   }
 }
--- src/scraper/scraper.controller.ts
+++ src/scraper/scraper.controller.ts
@@ -1,32 +1,53 @@
-import { Body, Controller, Post } from "@nestjs/common"
+import { Body, Controller, Post, Res } from "@nestjs/common"
+import { ConfigService } from "@nestjs/config"
+import { ApiCreatedResponse } from "@nestjs/swagger"
+import type { FastifyReply } from "fastify"
 import { ScrapeCommandDto } from "./dto/scrape-command.dto"
 import { ScrapeResultDto } from "./dto/scrape-result.dto"
 import { ScraperService } from "./scraper.service"
 
 @Controller("v1/scraper")
 export class ScraperController {
-  constructor(private readonly scraper: ScraperService) {}
+  private readonly quotaCostHeader: string
+  private readonly quotaName: string
+
+  constructor(config: ConfigService, private readonly scraper: ScraperService) {
+    this.quotaCostHeader = config.get<string>("QUOTA_COST_HEADER") as string
+    this.quotaName = config.get<string>("QUOTA_NAME", "Credits")
+  }
 
   @Post("csr")
-  postCSR(@Body() body: ScrapeCommandDto): Promise<ScrapeResultDto> {
-    return this.scraper.scrapeCSR(
-      body.url,
-      body.targets ?? [],
-      body.metadata ?? false,
-      body.blockAds ?? true,
-      body.headers,
-      body.httpProxy
-    )
+  @ApiCreatedResponse({ type: ScrapeResultDto })
+  async postCSR(@Body() body: ScrapeCommandDto, @Res() res: FastifyReply): Promise<void> {
+    const quotas = body.httpProxy ? 6 : 5
+    res
+      .header(this.quotaCostHeader, `${this.quotaName}=${quotas}`)
+      .send(
+        await this.scraper.scrapeCSR(
+          body.url,
+          body.targets ?? [],
+          body.metadata ?? false,
+          body.blockAds ?? true,
+          body.headers,
+          body.httpProxy
+        )
+      )
   }
 
   @Post("ssr")
-  postSSR(@Body() body: ScrapeCommandDto): Promise<ScrapeResultDto> {
-    return this.scraper.scrapeSSR(
-      body.url,
-      body.targets ?? [],
-      body.metadata ?? false,
-      body.headers,
-      body.httpProxy
-    )
+  @ApiCreatedResponse({ type: ScrapeResultDto })
+  async postSSR(@Body() body: ScrapeCommandDto, @Res() res: FastifyReply): Promise<void> {
+    const quotas = body.httpProxy ? 6 : 5
+    res
+      .header(this.quotaCostHeader, `${this.quotaName}=${quotas}`)
+      .send(
+        await this.scraper.scrapeSSR(
+          body.url,
+          body.targets ?? [],
+          body.metadata ?? false,
+          body.headers,
+          body.httpProxy
+        )
+      )
   }
 }
