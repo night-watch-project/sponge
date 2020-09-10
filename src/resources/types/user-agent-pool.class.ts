cfbae970bb810250e649310dc9e0fa2ebc0288e3
Lương Quang Mạnh <luongquangmanh85@gmail.com>
2020-09-11 00:07:42 +0700

    Fix error when USER_AGENT_POOL_URL not provided

--- src/resources/types/user-agent-pool.class.ts
+++ src/resources/types/user-agent-pool.class.ts
@@ -3,12 +3,19 @@ import { UserAgentEntity } from "./user-agent.interface"
 export class UserAgentPool {
     constructor(private readonly entities: UserAgentEntity[] = []) {}
 
-    public random(): string {
+    /*
+     * Pick a random user-agent, or return undefined if the pool is empty
+     */
+    public random(): string | undefined {
+        if (!this.entities.length) {
+            return undefined
+        }
+
         const idx = Math.floor(Math.random() * this.entities.length)
         return this.entities[idx].userAgent
     }
 
-    public extend(entities: UserAgentEntity[]) {
+    public extend(entities: UserAgentEntity[]): void {
         this.entities.push(...entities)
     }
 }
